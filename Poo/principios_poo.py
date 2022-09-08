b852d254c557db8189737ea32faa0f80e20c299d
NataVilla <nataye7@gmail.com>
2022-09-08 08:52:48 -0500

    Se crea segundo objeto y se modifica el codigo

--- Poo/principios_poo.py
+++ Poo/principios_poo.py
@@ -8,14 +8,19 @@ class Coche():
     ##Se establecen comportamientos (Metodos)
     ##self: hace referencia al propio objeto perteneciente a la clase.
 
-    def arrancar(self):
-        self.enmarcha = True
+    def arrancar(self, arrancamos):
 
-    def estado(self):
-        if(self.enmarcha):
+        self.enmarcha=arrancamos
+
+        if (self.enmarcha):
             return "El coche esta en marcha"
         else:
-            return "El coche esta parado"    
+            return "El coche esta parado"
+
+    def estado(self):
+        print("El coche tiene ", self.ruedas, " ruedas. Un ancho de ", self.anchoChasis,
+                " y un laro de ", self.largoChasis)
+
 
 ##Construccion de objetos
 ##Instaciar el objeto es cuando se asigna la clase al objeto que se esta creando.
@@ -25,5 +30,14 @@ print(miCoche.largoChasis)
 
 print("El coche tiene", miCoche.ruedas, "ruedas")
 
-#miCoche.arrancar()
-print(miCoche.estado())
+miCoche.arrancar(True)
+print(miCoche.arrancar(True))
+miCoche.estado()
+
+
+print("-----------------A continucion creamos el segundo objeto -----------------------------")
+
+miCochecito=Coche()
+miCochecito.arrancar(False)
+print(miCochecito.arrancar(False))
+miCochecito.estado()
