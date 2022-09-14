bac519cc8748434814135607a1f6e822f8ad3776
NataVilla <nataye7@gmail.com>
2022-09-14 08:45:00 -0500

    Se crea metodo chequeo interno y se encapsula

--- Poo/principios_poo.py
+++ Poo/principios_poo.py
@@ -1,31 +1,49 @@
+from ast import Return
+
+
 class Coche():
 ## Se crea constructor, el cual me da el estado inicial de la clase
     def __init__(self) -> None:
-        pass
-
+        
 ##Se crean las propiedades de la clase
-        self.largoChasis = 250
-        self.anchoChasis = 120
-        ##Se encapsula la variable ruedas para que no sea accesible desde el exterior
+        self.__largoChasis = 250
+        self.__anchoChasis = 120
         self.__ruedas =4
-        self.enmarcha=False
+        self.__enmarcha=False
 
     ##Se establecen comportamientos (Metodos)
     ##self: hace referencia al propio objeto perteneciente a la clase.
 
     def arrancar(self, arrancamos):
 
-        self.enmarcha=arrancamos
+        self.__enmarcha=arrancamos
+        if (self.__enmarcha):
+            chequeo=self.__chequeo_interno()
 
-        if (self.enmarcha):
+        if (self.__enmarcha and chequeo):
             return "El coche esta en marcha"
+
+        elif(self.__enmarcha and chequeo == False):
+            return "Algo a ido mal en el chequeo, no podemos arrancar"
+
         else:
             return "El coche esta parado"
 
     def estado(self):
-        print("El coche tiene ", self.ruedas, " ruedas. Un ancho de ", self.anchoChasis,
-                " y un laro de ", self.largoChasis)
+        print("El coche tiene ", self.__ruedas, " ruedas. Un ancho de ", self.__anchoChasis,
+                " y un laro de ", self.__largoChasis)
 
+    def __chequeo_interno(self):
+        print("Realizando cheque interno")
+
+        self.gasolina= "ok"
+        self.aceite="ok"
+        self.puertas="Cerradas"
+
+        if(self.gasolina=="ok" and self.aceite=="ok" and self.puertas=="Cerradas"):
+            return True
+        else:
+            return False
 
 ##Construccion de objetos
 ##Instaciar el objeto es cuando se asigna la clase al objeto que se esta creando.
