25abb6d92f355811f4d55af032a834df304becf7
NataVilla <nataye7@gmail.com>
2022-09-14 11:23:41 -0500

    Se agrega nuevas clases para hacer herencia multiple y sobreescribir un metodo

--- Poo/herencia.py
+++ Poo/herencia.py
@@ -1,3 +1,6 @@
+from cgi import print_arguments
+
+
 class Vehicles():
 
     def __init__(self, trademark, model):
@@ -20,9 +23,41 @@ class Vehicles():
         print("Trademark: ", self.trademark, "\nModel: ", self.model, "\nRun: ", self.run, "\nSpeed up: ", self.speed_up, "\nCurb: ", self.curb)
         
     
+class Furgoneta(Vehicles):
+    def carga(self, cargar):
+        self.cargado = cargar
+        if(self.cargado):
+            return "La furgoneta esta cargada"
+        else:
+            "La furgoneta no esta cargada"    
 
 class Motorcycle(Vehicles):
-    pass
+    h_horse = ""
+
+    def horse(self):
+        self.h_horse = "I'm making the horse"
+
+    def status(self):
+        print("Trademark: ", self.trademark, "\nModel: ", self.model, "\nRun: ", self.run, "\nSpeed up: ", self.speed_up, "\nCurb: ", self.curb, "\n", self.h_horse)    
+
+
+class electric_vehicles():
+    def __init__(self) -> None:
+        self.autonomy = 100
+
+    def charge(self):
+        self.charging = True
+
 
 myMotorcycle = Motorcycle("Honda", "CBR")
+myMotorcycle.horse()
 myMotorcycle.status()
+myFurgoneta = Furgoneta("Renault", "Kangaroo")
+myFurgoneta.speed()
+myFurgoneta.status()
+print(myFurgoneta.carga(True))
+
+class Electric_bicycle(electric_vehicles, Vehicles):
+    pass
+
+myBicycle = Electric_bicycle()
