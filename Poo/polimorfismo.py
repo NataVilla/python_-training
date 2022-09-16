0e802fe5395ccc157235d8eb943eeade00fcd4ba
NataVilla <nataye7@gmail.com>
2022-09-16 10:19:52 -0500

    Se crea nuevo archivo para polimorfismo

---
+++ Poo/polimorfismo.py
@@ -0,0 +1,24 @@
+class Coche():
+
+    def desplazamiento(self):
+        print("Me desplazon en 4 ruedas")
+
+class Moto():
+
+    def desplazamiento(self):
+        print("Me desplazo en 2 ruedas")
+
+class Camion():
+
+    def desplazamiento(self):
+        print("Me desplazo en 6 ruedas")
+
+
+""" Esta funcion se encarga de llamar a cada metodo desplazamiento de las clases """
+def desplazamiento(vehiculo):
+    vehiculo.desplazamiento()
+
+
+miVehiculo = Moto()
+desplazamiento(miVehiculo)
+        
