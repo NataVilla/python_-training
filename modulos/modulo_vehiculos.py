cbc293fcb2a65f126393f17df50726799a5e74fe
NataVilla <nataye7@gmail.com>
2022-09-19 14:54:02 -0500

    "Se reutiliza el codigo de archivo de herencia"

---
+++ modulos/modulo_vehiculos.py
@@ -0,0 +1,47 @@
+
+class Vehicles():
+
+    def __init__(self, trademark, model):
+        self.trademark = trademark
+        self.model = model
+        self.run = False
+        self.speed_up = False
+        self.curb = False
+
+    def start(self):
+        self.run = True
+
+    def speed(self):
+        self.speed_up = True
+
+    def stop(self):
+        self.curb = True
+
+    def status(self):
+        print("Trademark: ", self.trademark, "\nModel: ", self.model, "\nRun: ", self.run, "\nSpeed up: ", self.speed_up, "\nCurb: ", self.curb)
+        
+    
+class Furgoneta(Vehicles):
+    def carga(self, cargar):
+        self.cargado = cargar
+        if(self.cargado):
+            return "La furgoneta esta cargada"
+        else:
+            "La furgoneta no esta cargada"    
+
+class Motorcycle(Vehicles):
+    h_horse = ""
+
+    def horse(self):
+        self.h_horse = "I'm making the horse"
+
+    def status(self):
+        print("Trademark: ", self.trademark, "\nModel: ", self.model, "\nRun: ", self.run, "\nSpeed up: ", self.speed_up, "\nCurb: ", self.curb, "\n", self.h_horse)    
+
+
+class electric_vehicles():
+    def __init__(self) -> None:
+        self.autonomy = 100
+
+    def charge(self):
+        self.charging = True
