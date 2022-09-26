71e45ee7a8260fdf4be9e6b18df81881f06b40d8
NataVilla <nataye7@gmail.com>
2022-09-26 10:17:57 -0500

    Se escribe nueva linea en el archivo ya exitente

--- archivos_externos/manejo_archivos.py
+++ archivos_externos/manejo_archivos.py
@@ -8,11 +8,14 @@ from io import open
 
 # archivo_texto.close()
 
-archivo_texto = open("archivo.txt", "r")
+# archivo_texto = open("archivo.txt", "r")
 
-linea_texto = archivo_texto.readlines()
+# linea_texto = archivo_texto.readlines()
 
+# archivo_texto.close()
+# print(linea_texto)
+
+
+archivo_texto = open("archivo.txt", "a")
+archivo_texto.write("\n Siempre es una buena ocasion para estudiar Python")
 archivo_texto.close()
-print(linea_texto)
-
-
